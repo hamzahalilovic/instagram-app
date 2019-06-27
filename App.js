ee3b5ce168bcab60e3475dc83283864ef1ff54e6
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2019-06-27 03:29:26 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component{
+  render (){
+    return (
+      <View style={StyleSheet.container}>
+
+
+      </View>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  
+})
